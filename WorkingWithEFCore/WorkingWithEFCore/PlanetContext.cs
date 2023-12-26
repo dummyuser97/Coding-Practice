fbc51042e88c8ecf0673b31c2ce67336e04e7d12
Satish <dummyemail20051997@gmail.com>
2023-12-26 20:55:35 +0530

    Added BuberBeakfast - web API project

---
+++ WorkingWithEFCore/WorkingWithEFCore/PlanetContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithEFCore
+{
+    public class PlanetContext : DbContext
+    {
+        public DbSet<Planet> Planets { get; set; }
+
+    }
+}
